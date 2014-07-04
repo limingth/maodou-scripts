3e3abe8fc2c5d36a0c47c8cc63000a32a05c8116
root <root@AY140703094435557aacZ.(none)>
2014-07-05 05:23:04 +0800

    add setup-ubuntu.sh

--- setup-maodou-server.sh
+++ setup-maodou-server.sh
@@ -1,6 +1,3 @@
-# modify PS1
-echo export PS1=\"root@182.92.160.153 \\w\$ \" >> ~/.bash_profile 
-. ~/.bash_profile
+# install webrtc 
 
-# install curl
-sudo apt-get install -y curl
+# install pp2code.com
---
+++ setup-ubuntu.sh
@@ -0,0 +1,47 @@
+# wget 'raw path of setup-ubuntu.sh'
+# . setup-ubuntu.sh
+
+# modify PS1
+echo export PS1=\"root@182.92.160.153 \\w\$ \" >> ~/.bashrc
+. ~/.bashrc
+
+# install curl
+sudo apt-get install curl -y
+
+# install git
+sudo apt-get install git -y
+
+# install vnc4server
+sudo apt-get install vnc4server -y
+vnc4server -kill :1
+vnc4server -geometry 800x600 :1
+
+# install guacamole
+sudo apt-get update
+sudo apt-get install guacamole -y
+sudo apt-get install guacamole-tomcat -y
+cat /etc/guacamole/user-mapping.xml 
+sudo service tomcat6 restart
+echo http://182.92.160.153:8080/guacamole/
+
+# install chrome
+sudo apt-get install chromium-browser -y
+
+# install gedit
+sudo apt-get install gedit -y
+
+# install firefox
+sudo apt-get install firefox -y
+
+# install x-window
+sudo apt-get install x-window-system-core -y
+
+# after install gdm, every windows has a head panel
+sudo apt-get install gdm -y
+
+# without install ubuntu desktop, no Chinese fonts
+sudo apt-get install ubuntu-desktop -y
+
+
+
+
