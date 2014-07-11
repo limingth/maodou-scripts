b92f16057a75eb2ec54bcca8242519376a003284
Li Ming <limingth@gmail.com>
2014-07-11 17:10:05 +0800

    Update maodou-server-setup.sh

--- maodou-server-setup.sh
+++ maodou-server-setup.sh
@@ -10,4 +10,15 @@ sudo apt-key add rabbitmq-signing-key-public.asc
 apt-get update
 sudo apt-get install rabbitmq-server -y
 
+# install setuptools for python
+wget "https://bitbucket.org/pypa/setuptools/get/default.tar.gz#egg=setuptools-dev" --no-check-certificate
+tar -xzvf default.tar.gz
+cd pypa-setuptools-9616be97e6a7/
+sudo python setup.py install
+
+# install Gateone
+git clone https://github.com/liftoff/GateOne.git
+cd GateOne
+python setup.py install
+
 # install pp2code.com
