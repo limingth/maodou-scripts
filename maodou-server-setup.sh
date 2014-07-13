aa6e65a529cf8caf53ab0614ecfa39a4a498c6eb
Li Ming <limingth@gmail.com>
2014-07-14 00:18:42 +0800

    Update maodou-server-setup.sh

--- maodou-server-setup.sh
+++ maodou-server-setup.sh
@@ -27,11 +27,19 @@ git clone https://github.com/liftoff/GateOne.git
 cd GateOne
 python setup.py install
 
+# install java7 (java version "1.7.0_60")
+sudo add-apt-repository ppa:webupd8team/java
+sudo apt-get update
+sudo apt-get install oracle-java7-installer
+java -version
+
 # install elasticsearch
 wget https://download.elasticsearch.org/elasticsearch/elasticsearch/elasticsearch-1.1.1.deb
 sudo dpkg -i elasticsearch-1.1.1.deb
 # start elasticsearch service 
 sudo service elasticsearch restart
+curl -XGET 'http://localhost:9200'
 # creating the index
 bundle exec rake environment elasticsearch:import:model CLASS='User' FORCE=true
+
 
