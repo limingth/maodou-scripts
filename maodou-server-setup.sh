ff0db5e1e0af59f03d510a2cdd8c3b8377a4108b
Li Ming <limingth@gmail.com>
2014-08-11 08:50:38 +0800

    Update maodou-server-setup.sh

--- maodou-server-setup.sh
+++ maodou-server-setup.sh
@@ -2,7 +2,12 @@
 #git clone http://github.com/limingth/webrtc.git
 
 # install mongdb
+# http://docs.mongodb.org/manual/tutorial/install-mongodb-on-ubuntu/
 sudo apt-get install mongodb -y
+sudo apt-key adv --keyserver hkp://keyserver.ubuntu.com:80 --recv 7F0CEB10
+echo 'deb http://downloads-distro.mongodb.org/repo/ubuntu-upstart dist 10gen' | sudo tee /etc/apt/sources.list.d/mongodb.list
+sudo apt-get update
+sudo apt-get install mongodb-org -y
 
 # install rabbitmq-server
 wget http://www.rabbitmq.com/rabbitmq-signing-key-public.asc
